060348b18eb6d544937a82bd99f42ea8935a751c
marshal <1337marshal1337@gmail.com>
2025-03-30 21:43:18 +0300

    upd

--- lerify/app/(site)/page.tsx
+++ lerify/app/(site)/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import PageContent from "@/components/PageContent";
 
+
 export const revalidate = 0;
 
 export default async function Home() {
