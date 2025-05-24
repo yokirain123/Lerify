187764012cd0183acf57780d4db8535eeaa00876
marshal <1337marshal1337@gmail.com>
2025-05-25 01:10:44 +0300

    test 2

--- lerify/tailwind.config.ts
+++ lerify/tailwind.config.ts
@@ -1,13 +1,12 @@
-import {nextui} from '@nextui-org/theme';
-import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
-  plugins: [nextui()],
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", // fixed!
   ],
   theme: {
     extend: {
@@ -17,9 +16,11 @@ const config: Config = {
         "bg-color": "var(--bg-color)",
       },
       dropShadow: {
-        '3xl': '0px 0px 15px #88b4fc',
-      }
+        "3xl": "0px 0px 15px #88b4fc",
+      },
     },
   },
+  plugins: [nextui()],
 };
+
 export default config;
